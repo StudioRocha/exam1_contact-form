02f5bca5d7471fd509c8f0f6a36981822adfc0d3
kensaku <x.studio.rocha@gmail.com>
2025-09-04 00:57:26 +0900

    0904

--- src/resources/views/admin/contacts/index.blade.php
+++ src/resources/views/admin/contacts/index.blade.php
@@ -46,9 +46,7 @@
     @endif
 
     <div class="admin__heading">
-        <div class="admin__pagination admin__pagination--top">
-            {{ $contacts->links() }}
-        </div>
+        <x-pagination :paginator="$contacts" align="right" />
     </div>
 
     <div class="admin__table-wrapper">
@@ -74,29 +72,7 @@
                     </td>
                 </tr>
 
-                <div id="modal-{{ $contact->id }}" class="admin__modal" aria-hidden="true">
-                    <div class="admin__modal-backdrop" data-modal-close></div>
-                    <div class="admin__modal-dialog" role="dialog" aria-modal="true">
-                        <button class="admin__modal-close" type="button" data-modal-close>×</button>
-                        <div class="admin__modal-body">
-                            <dl class="admin__detail-list">
-                                <dt>お名前</dt><dd>{{ $contact->last_name }}　{{ $contact->first_name }}</dd>
-                                <dt>性別</dt><dd>{{ ['','男性','女性','その他'][$contact->gender] }}</dd>
-                                <dt>メールアドレス</dt><dd>{{ $contact->email }}</dd>
-                                <dt>電話番号</dt><dd>{{ $contact->tel }}</dd>
-                                <dt>住所</dt><dd>{{ $contact->address }}</dd>
-                                <dt>建物名</dt><dd>{{ $contact->building ?? '-' }}</dd>
-                                <dt>お問い合わせの種類</dt><dd>{{ $categories[$contact->category_id] ?? 'その他' }}</dd>
-                                <dt>お問い合わせ内容</dt><dd>{{ $contact->detail }}</dd>
-                            </dl>
-                            <form method="POST" action="{{ route('admin.contacts.destroy', $contact->id) }}" onsubmit="return confirm('削除しますか？');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="admin__button admin__button--danger">削除</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <x-admin.contact-modal :contact="$contact" :categories="$categories" />
             @endforeach
             </tbody>
         </table>
